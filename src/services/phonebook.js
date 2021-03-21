939e5e1abbb5d0c80e9147da41b6fc18359257cf
Atakan Tekoğlu <atakantekoglu@gmail.com>
2021-03-22 00:52:30 +0300

    changed base url

--- src/services/phonebook.js
+++ src/services/phonebook.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://expressbackend11.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)

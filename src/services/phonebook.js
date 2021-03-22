46fa20c9921830a1bd5f659d3150960f5726a5e0
Atakan Tekoğlu <atakantekoglu@gmail.com>
2021-03-23 00:47:28 +0300

    package.json and service file phonebook

--- src/services/phonebook.js
+++ src/services/phonebook.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
+//!Now HTTP GET requests to the address www.serversaddress.com/index.html or www.serversaddress.com will show the React frontend. GET requests to the address www.serversaddress.com/api/notes will be handled by the backend's code.Because of our situation, both the frontend and the backend are at the same address, we can declare baseUrl as a relative URL. This means we can leave out the part declaring the server.
 const baseUrl = '/api/persons'
+
+//const baseUrl = 'https://expressbackend11.herokuapp.com/api/persons' bu da olabilir
 
 const getAll = () => {
     const request = axios.get(baseUrl)
@@ -14,7 +17,6 @@ const create = (newObj) =>{
 
 const deletePhone = (id) => {
     const request = axios.delete(`${baseUrl}/${id}`)
-    
     return request.then(response => response.status)
 }
 /*
